e7cc138966454b6e5cd5580c82bc87e34a596c6f
numshv <19623011@std.stei.itb.ac.id>
2024-12-03 19:51:22 +0700

    feat: add Proyek

---
+++ src/entities/Proyek.py
@@ -0,0 +1,11 @@
+class Proyek:
+    def __init__(self, idProyek=None, judulProyek="", descProyek="", progressProyek="", biayaProyek=None, estimasiBiayaProyek=None, tanggalMulaiProyek=None, tanggalSelesaiProyek=None, statusProyek=""):
+        self.idProyek = idProyek
+        self.judulProyek = judulProyek
+        self.descProyek = descProyek
+        self.progressProyek = progressProyek
+        self.biayaProyek = biayaProyek
+        self.estimasiBiayaProyek = estimasiBiayaProyek
+        self.tanggalMulaiProyek = tanggalMulaiProyek
+        self.tanggalSelesaiProyek = tanggalSelesaiProyek
+        self.statusProyek = statusProyek
