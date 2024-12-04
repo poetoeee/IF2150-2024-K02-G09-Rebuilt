c3f0c949dadf711475550676efe13398fefbc241
19623023 <19623023@std.stei.itb.ac.id>
2024-12-04 19:12:05 +0700

    Tugas  updated

--- src/entities/TugasProyek.py
+++ src/entities/TugasProyek.py
@@ -1,39 +1,39 @@
 class TugasProyek:
-    def __init__(self, tugas_id = None, tugas_nama = "", tugas_deskripsi = "", tugas_status = "Not Started", proyek_id = None):
-        self.tugas_id = tugas_id
-        self.tugas_nama = tugas_nama
-        self.tugas_deskripsi = tugas_deskripsi
-        self.tugas_status = tugas_status
-        self.proyek_id = proyek_id
+    def __init__(self, idTugas = None, judulTugas = "", descTugas = "", biayaTugas = None, statusProyek = ""):
+        self.idTugas = idTugas
+        self.judulTugas = judulTugas
+        self.descTugas = descTugas
+        self.biayaTugas = biayaTugas
+        self.statusProyek = statusProyek
         
 
-    def get_tugas_id(self):
-        return self.tugas_id
+    def get_idTugas(self):
+        return self.idTugas
     
-    def set_tugas_id(self, tugas_id):
-        self.tugas_id = tugas_id
+    def set_tugas_id(self,idTugas):
+        self.idTugas = idTugas
 
     def get_tugas_nama(self):
         return self.get_tugas_nama
         
-    def set_tugas_nama(self, tugas_nama):
-        self.tugas_nama = tugas_nama
+    def set_judulTugas(self, judulTugas):
+        self.judulTugas= judulTugas
 
-    def get_tugas_deskripsi(self):
-        return self.tugas_deskripsi
+    def get_descTugas(self):
+        return self.descTugas
 
-    def set_tugas_deskripsi(self, tugas_deskripsi):
-        self.tugas_deskripsi = tugas_deskripsi
+    def set_descTugas(self,descTugas):
+        self.descTugas = descTugas
 
-    def get_tugas_status(self):
-        return self.tugas_status 
+    def get_biayaTugas(self):
+        return self.biayaTugas
 
-    def set_tugas_status(self,tugas_status):
-        self.tugas_status = tugas_status
+    def set_biayaTugas(self, biayaTugas):
+        self.biayaTugas = biayaTugas
 
-    def get_proyek_id(self):
-        return self.proyek_id
+    def get_statusProyek(self):
+        return self.statusProyek
 
-    def set_proyek_id(self, proyek_id):
-        self.proyek_id = proyek_id
+    def set_statusProyek(self, statusProyek):
+        self.statusProyek = statusProyek
 
