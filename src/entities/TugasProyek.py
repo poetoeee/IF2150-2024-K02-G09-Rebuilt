2dd2d0a00f4c07c994e8ba254bd89122d760b169
Starath <164960528+Starath@users.noreply.github.com>
2024-12-05 09:36:03 +0700

    refactor(TugasProyek): added atribute

--- src/entities/TugasProyek.py
+++ src/entities/TugasProyek.py
@@ -5,16 +5,17 @@ class TugasProyek:
         self.descTugas = descTugas
         self.biayaTugas = biayaTugas
         self.statusProyek = statusProyek
+        self.idProyekOfTugas = idProyekOfTugas
         
 
     def get_idTugas(self):
         return self.idTugas
     
-    def set_tugas_id(self,idTugas):
+    def set_idTugas(self,idTugas):
         self.idTugas = idTugas
 
-    def get_tugas_nama(self):
-        return self.get_tugas_nama
+    def get_judulTugas(self):
+        return self.get_judulTugas
         
     def set_judulTugas(self, judulTugas):
         self.judulTugas= judulTugas
@@ -36,4 +37,10 @@ class TugasProyek:
 
     def set_statusProyek(self, statusProyek):
         self.statusProyek = statusProyek
+
+    def get_idProyekOfTugas(self):
+        return self.idProyekOfTugas
+
+    def set_idProyekOfTugas(self, idProyekOfTugas):
+        self.idProyekOfTugas = idProyekOfTugas
 
