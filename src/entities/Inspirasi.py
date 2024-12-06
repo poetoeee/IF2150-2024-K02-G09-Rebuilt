7a963a77fee7d78b68f78b16a8d7b4a55bd545c8
Muhammad Edo Raduputu Aprima <19623236@std.stei.itb.ac.id>
2024-12-06 15:25:02 +0700

    feat(Inspirasi): added entitiy Inspirasi

---
+++ src/entities/Inspirasi.py
@@ -0,0 +1,37 @@
+class Isnpirasi:
+    def __init__(self, idInspirasi=None, judulInspirasi="", descInspirasi="", imageInspirasi="", linkInspirasi=""):
+        self.idInspirasi = idInspirasi
+        self.judulInspirasi = judulInspirasi
+        self.descInspirasi = descInspirasi
+        self.imageInspirasi = imageInspirasi
+        self.linkInspirasi = linkInspirasi
+        
+    def getIdInspirasi(self):
+        return self.idInspirasi
+
+    def setidInspirasi(self, idInspirasi):
+        self.idInspirasi = idInspirasi
+
+    def getjudulInspirasi(self):
+        return self.judulInspirasi
+
+    def setjudulInspirasi(self, judulInspirasi):
+        self.judulInspirasi = judulInspirasi
+        
+    def getdescInspirasi(self):
+        return self.descInspirasi
+
+    def setdescInspirasi(self, descInspirasi):
+        self.descInspirasi = descInspirasi
+
+    def getimageInspirasi(self):
+        return self.imageInspirasi
+
+    def setimageInspirasi(self, imageInspirasi):
+        self.imageInspirasi = imageInspirasi
+
+    def getlinkInspirasi(self):
+        return self.linkInspirasi
+
+    def setlinkInspirasi(self, linkInspirasi):
+        self.linkInspirasi = linkInspirasi
