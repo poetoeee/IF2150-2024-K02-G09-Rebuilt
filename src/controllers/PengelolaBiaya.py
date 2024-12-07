26e6cc130da8c9ea57a5ed78dd0ccd61c6d7955d
19623023 <19623023@std.stei.itb.ac.id>
2024-12-07 17:48:35 +0700

    Pengelola Biaya updated

--- src/controllers/PengelolaBiaya.py
+++ src/controllers/PengelolaBiaya.py
@@ -1,2 +1,40 @@
+from ../entities/Biaya import Biaya
+from ../database/db_connection import get_connection
+
 class PengelolaBiaya:
+    def addBiaya(self, biaya):
+        connection = get_connection()
+        if not connection:
+            print("Failed to get database connection.")
+            return False
     
+        try:
+            cursor = connection.cursor()
+            query = '''
+                    INSERT INTO t_biaya (
+                    namaBarangBiaya,
+                    keteranganBiaya,
+                    hargaSatuanBiaya,
+                    quantityBiaya,
+                    totalBiaya
+                ) VALUES (?, ?, ?, ?)
+            '''
+
+            values = (
+                tugas.getJudulTugas(),
+                tugas.getDescTugas(),
+                tugas.getBiayaTugas(),
+                tugas.getStatusTugas(),
+            )
+            cursor.execute(query, values)
+            connection.commit()
+            return True
+        
+        except Exception as err:
+            print(f"Error creating biaya: {err}")
+            return False
+        
+        finally:
+            if 'cursor' in locals():
+                cursor.close()
+            connection.close()
