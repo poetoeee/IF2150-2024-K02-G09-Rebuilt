7bdce3014f0758c7edf54ffe1587358fec6b5d75
19623023 <19623023@std.stei.itb.ac.id>
2024-12-07 17:38:31 +0700

    Updated Pengelola biaya

--- src/controllers/PengelolaBiaya.py
+++ src/controllers/PengelolaBiaya.py
@@ -1,2 +1,10 @@
+from entities.Biaya import Biaya
+from database.db_connection import get_connection
+
 class PengelolaBiaya:
-    
+    from entities.Biaya import Biaya
+from database.db_connection import get_connection
+
+class PengelolaBiaya:
+    def deleteBiaya():
+        connection = get_connection()
