7d931598ad037d5478bd05e826857d9c0b8b613f
19623023 <19623023@std.stei.itb.ac.id>
2024-12-07 17:58:29 +0700

    Pengelola Biaya updated

--- src/controllers/PengelolaBiaya.py
+++ src/controllers/PengelolaBiaya.py
@@ -1,5 +1,5 @@
-from ../entities/Biaya import Biaya
-from ../database/db_connection import get_connection
+from entities.Biaya import Biaya
+from database.db_connection import get_connection
 
 class PengelolaBiaya:
     def addBiaya(self, biaya):
@@ -34,6 +34,28 @@ class PengelolaBiaya:
             print(f"Error creating biaya: {err}")
             return False
         
+        finally:
+            if 'cursor' in locals():
+                cursor.close()
+            connection.close()
+
+def deleteBiaya(self, idBiayaInput):
+        connection = get_connection()
+        if not connection:
+            print("Failed to get database connection.")
+            return False
+    
+        try:
+            cursor = connection.cursor()
+            query = "DELETE FROM t_biaya WHERE idBiaya = ?"
+            cursor.execute(query, (idBiayaInput,))
+            connection.commit()
+            return cursor.rowcount > 0
+        
+        except Exception as err:
+            print(f"Error deleting Biaya: {err}")
+            return False
+
         finally:
             if 'cursor' in locals():
                 cursor.close()
